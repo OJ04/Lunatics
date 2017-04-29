434af9ba26bbf269a6df89ed542905e3b191727d
Timo <timo@overmark.org>
2017-04-29 18:11:19 +0300

    Changed the feedback from constant vibration to pulses. Measurement only using one sensor, feedback with all motors.

--- glove/glove.ino
+++ glove/glove.ino
@@ -8,18 +8,17 @@ int btn_t;      //time since state change
 
 //vibration state
 boolean button_state = false;
-boolean vibrate_on = false;
+boolean vibrate_on = true;
 
 //define vibrators: pin, last value given to the pin
-int vibrator_1[] = {3,0};
-int vibrator_2[] = {10,0};
-int vibrator_3[] = {9,0};
-
+int vibrator_right[] = {3,0};
+int vibrator_front[] = {10,0};
+int vibrator_left[] = {9,0};
 
 // defines sonar pins trigger, echo respectively
-int sonar_1[] = {8,7};
-int sonar_2[] = {5,6};
-int sonar_3[] = {12,11};
+int sonar_right[] = {8,7};
+int sonar_front[] = {5,6};
+int sonar_left[] = {12,11};
 
 //max distance of measurement
 const int max_dist = 412; //change treshold 10cm
@@ -32,28 +31,25 @@ int buffer_index = 0;
 //buffers containing last measurements(not in order)
 //measurements are not in order to avoid shifting each buffer everytime a new
 //reading is measured, which would be slow
-int d_buffer_1[buffer_len];
-int d_buffer_2[buffer_len];
-int d_buffer_3[buffer_len];
+int d_buffer_right[buffer_len];
+int d_buffer_front[buffer_len];
+int d_buffer_left[buffer_len];
 
-//buffers containing changes since last (about) 2 seconds (a loop takes about 83ms)
-const int c_treshold = 15; //change treshold 10cm
-const int c_buffer_length = 28;
-int c_buffer_1[c_buffer_length];
-int c_buffer_2[c_buffer_length];
-int c_buffer_3[c_buffer_length];
-int c_buffer_index = 0;
+//time since last change
+unsigned long pulse_interval = 0;
+unsigned long pulse_start_time = 0;
+const int pulse_duration = 100;
 
 void setup() {
   //init sonar pins
-  initSonar(sonar_1);
-  initSonar(sonar_2);
-  initSonar(sonar_3);
+  initSonar(sonar_right);
+  initSonar(sonar_front);
+  initSonar(sonar_left);
 
   //set vibrator pins as output
-  pinMode(vibrator_1[0],OUTPUT);
-  pinMode(vibrator_2[0],OUTPUT);
-  pinMode(vibrator_3[0],OUTPUT);
+  pinMode(vibrator_right[0],OUTPUT);
+  pinMode(vibrator_front[0],OUTPUT);
+  pinMode(vibrator_left[0],OUTPUT);
 
   //set sonar powering pin as output and set high to power the sonars
   pinMode(SONAR_PWR_PIN, OUTPUT);
@@ -71,27 +67,22 @@ void loop() {
   //delay(10);
   //read distances from sonars and print them to ouput and put them to
   //corresponding buffers
-  int d = getDistance(sonar_1);
-  d_buffer_1[buffer_index] = d;
-  Serial.print(d);
-  Serial.print(" ");
-  d = getDistance(sonar_2);
-  d_buffer_2[buffer_index] = d;
-  Serial.print(d);
-  Serial.print(" ");
-  d = getDistance(sonar_3);
-  d_buffer_3[buffer_index] = d;
-  Serial.println(d);
+  int d = getDistance(sonar_right);
+  d_buffer_right[buffer_index] = d;
+  //Serial.print(d);
+  //Serial.print(" ");
+  d = getDistance(sonar_front);
+  d_buffer_front[buffer_index] = d;
+  //Serial.print(d);
+  //Serial.print(" ");
+  d = getDistance(sonar_left);
+  d_buffer_left[buffer_index] = d;
+  //Serial.println(d);
 
-  Serial.println(read_button());
+  //Serial.println(read_button());
 
   //increase index to next slot keeping in range 0-buffer_len-1
   buffer_index = (buffer_index+1)%buffer_len;
-
-  c_buffer_index = (c_buffer_index+1)%c_buffer_length;
-  c_buffer_1[c_buffer_index] = average(d_buffer_1);
-  c_buffer_2[c_buffer_index] = average(d_buffer_2);
-  c_buffer_3[c_buffer_index] = average(d_buffer_3);
 
   //when putton is released, change vibrate on/off
   if(button_state == true && read_button() == false) {
@@ -102,19 +93,53 @@ void loop() {
     button_state = read_button();
   }
 
+  pulse_interval = distToInterval(average(d_buffer_front));
+
+
   //vibrate in "on" state
   if(vibrate_on) {
 
     //calculate vibration strength from average of last measuremnts
     //and update vibrators accordingly
-    vibrate(vibrator_1,distToStrength(average(d_buffer_1)));
-    vibrate(vibrator_2,distToStrength(average(d_buffer_2)));
-    vibrate(vibrator_3,distToStrength(average(d_buffer_3)));
-  }
+    //vibrate(vibrator_right,(distToStrength(average(d_buffer_right))/2));
+    //vibrate(vibrator_front,distToStrength(average(d_buffer_front)));
+    //vibrate(vibrator_left,(distToStrength(average(d_buffer_left))/2));
+    if(pulse_interval > 0) {
+        Serial.print("Interval: ");
+        Serial.println(pulse_interval);
+
+      //start pulsing
+      if(pulse_start_time == 0) pulse_start_time = millis();
+      if((millis()-pulse_start_time) > (pulse_interval+pulse_duration)) {
+        vibrate(vibrator_right,0);
+        vibrate(vibrator_front,0);
+        vibrate(vibrator_left,0);
+        Serial.print("Start time:");
+        Serial.print(pulse_start_time);
+        Serial.print(", millis:");
+        Serial.print(millis());
+        Serial.println("End pulse.");
+        pulse_start_time = millis();
+      }
+      else if((millis()-pulse_start_time) > pulse_interval) {
+        vibrate(vibrator_right,250);
+        vibrate(vibrator_front,250);
+        vibrate(vibrator_left,250);
+        Serial.println("Start pulse");
+      }
+    }
+    else {
+      pulse_start_time = 0;
+      vibrate(vibrator_right,0);
+      vibrate(vibrator_front,0);
+      vibrate(vibrator_left,0);
+
+    }
+  } 
   else {
-    vibrate(vibrator_1,0);
-    vibrate(vibrator_2,0);
-    vibrate(vibrator_3,0);
+    vibrate(vibrator_right,0);
+    vibrate(vibrator_front,0);
+    vibrate(vibrator_left,0);
   }
 }
 
@@ -135,6 +160,20 @@ int distToStrength(int d)
   return min(max((20000/(d+20)-50),0),255);
 }
 
+//convert distance to pulse interval
+int distToInterval(int d)
+{
+  //if no reading set strength to 0
+  if(d == 0)
+  {
+    return 0;
+  }
+
+  if(d < 150) {
+    return (d*8);
+  }
+}
+
 //update vibrator with given strength
 void vibrate(int vibrator[], int strength)
 {
@@ -146,6 +185,7 @@ void vibrate(int vibrator[], int strength)
     analogWrite(vibrator[0],255);
     delay(1);
   }
+
 
   //write strength to the vibrator pin and
   //to the vibrator array
@@ -164,24 +204,6 @@ int average(int bufr[])
     if(bufr[n] < max_dist) sum += bufr[n];
   }
   return sum/amt;
-}
-
-boolean significant_change(int bufr[])
-{
-  int min_value = bufr[0];
-  int max_value = bufr[0];
-
-  for(int n=1;n<c_buffer_length;n++)
-  {
-    min_value = min(bufr[n],min_value);
-    max_value = max(bufr[n],max_value);
-  }
-
-  if((max_value-min_value) > c_treshold) {
-    Serial.print("significant: ");
-    Serial.println((max_value-min_value));
-  }
-  else return false;
 }
 
 //set sonar pins output and input properly
